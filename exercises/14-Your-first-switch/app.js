e6435f9cfe80ece0abb31f8f3a97b463a4a22fa1
mlanorero <mlanorero@gmail.com>
2020-06-17 12:01:41 +0000

    avanzando

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,14 +1,31 @@
-function getColor(selection)
-{
-	switch(selection){
-		//Add more options here
+function getColor(colorname)
+{   
+	switch(colorname.toLocaleLowerCase()){
+        //Add more options here
+        case 0:
+            colorname = 'red';
+            return true;
+            break;
+        case 1:
+            colorname = 'green';
+            return true;
+            break;
+        case 2:
+            colorname = 'blue';
+            return true;
+            break;
 	    default :
 	    	return false;//returns false because the user picked an unavailable color
 	    break;               
-	}
+    }
+    return colorname;
 }
 
-var colorname = window.prompt('What color do you want?');
+var colorname = prompt('What color do you want?');
 var isAvailable = getColor(colorname);
-if(isAvailable) console.log('Good news! That color is available');
-else console.log('We are sorry, that color is not available');
+if(isAvailable === true ) { 
+    console.log('Good news! That color is available');
+}
+else  {
+console.log('We are sorry, that color is not available');
+}
--- exercises/18-Create-a-for-loop/app.js
+++ exercises/18-Create-a-for-loop/app.js
@@ -1,6 +1,13 @@
 /* This exercise should be moved before 17-Your first loop? */
 
 // Declare and write your function here:
+function standardsMaker(){
+    for(let i = 0; i < 300; i++){
+       console.log( "I will write questions if I'm stuck" )
+    }
+    return console.log;
+}
+
 
 
 // Calling the function:
